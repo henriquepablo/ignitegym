4f3bba4862bd00252fa7951c961c57b204883002
henriquepablo <pablo269704@gmail.com>
2025-05-30 19:30:33 -0300

    chore: config status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
 
 export default function App() {
@@ -6,7 +6,13 @@ export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        backgroundColor: "#202024"
+      }}>
+        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
       {
         fontsLoaded ?  <Text>Home</Text> : <View />
       }
