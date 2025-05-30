e146883d7bd2c0cc3059de8622cae125f6066df1
henriquepablo <pablo269704@gmail.com>
2025-05-30 19:45:59 -0300

    chore: config gluestack

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
-import { Text, View, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
+import { GluestackUIProvider, Text } from "@gluestack-ui/themed";
 
 export default function App() {
   
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
 
   return (
-    <View style={{
-        flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center',
-        backgroundColor: "#202024"
-      }}>
-        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {
-        fontsLoaded ?  <Text>Home</Text> : <View />
-      }
-    </View>
+    <GluestackUIProvider>
+      <View style={{
+          flex: 1, 
+          justifyContent: 'center', 
+          alignItems: 'center',
+          backgroundColor: "#202024"
+        }}>
+          <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+        {
+          fontsLoaded ?  <Text>Home</Text> : <View />
+        }
+      </View>
+    </GluestackUIProvider>
   );
 }
 
